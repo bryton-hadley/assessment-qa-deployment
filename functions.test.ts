01b353a6a1809feef13bf8bad8f7fe0f4b958473
Bryton Haldey <bryton.hadley@gmail.com>
2022-09-23 12:36:32 -0600

    need to go back and get it to conncet

---
+++ functions.test.ts
@@ -0,0 +1,11 @@
+const {shuffleArray} = require('./utils')
+
+describe('shuffleArray should', () => {
+    
+    test('Return an Array', () => {
+
+        expect((shuffleArray).toBe([]))
+    })
+
+    test('Should return an legth')
+})
