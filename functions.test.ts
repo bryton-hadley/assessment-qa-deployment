dfc8bd0228e7df59fd21128e6b3a7f3ee4d1de5a
Bryton Haldey <bryton.hadley@gmail.com>
2022-09-23 16:41:45 -0600

    finsihing uo the function.test

--- functions.test.ts
+++ functions.test.ts
@@ -2,10 +2,39 @@ const {shuffleArray} = require('./utils')
 
 describe('shuffleArray should', () => {
     
-    test('Return an Array', () => {
+    // test('Return an Array', () => {
+    //     let arr = [1,2,3,4,5]
+        
+    //     expect((shuffleArray(arr)).toBe('array'))
+    // })
 
-        expect((shuffleArray).toBe([]))
-    })
+    test('check the lenght and see if they are the same', () => {
 
-    test('Should return an legth')
+        let arr = [1,2,3,4,5]
+        let results = shuffleArray(arr)
+
+        expect(arr.length).toBe(results.length)   
+     })
+
+     test('return an array with same times', () => {
+        //create array
+        //run though shuffle array function
+        //Check to make sure all original values are included in new array 
+        let arr = [1,2,3,4,5]
+        let results = shuffleArray(arr)
+
+        let myArr = true
+
+        for(let i = 0; i < results.length; i++){
+           
+            if(arr.includes(results[i]) === false){
+
+               
+                myArr = false
+                
+                return
+            }
+        }
+        expect(myArr).toBe(true)
+     })
 })
