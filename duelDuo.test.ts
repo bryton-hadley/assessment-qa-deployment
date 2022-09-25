8d09b1b06c37833d1265a869658ebef3c2490dc3
Bryton Haldey <bryton.hadley@gmail.com>
2022-09-24 19:06:10 -0600

    still working on the automation tests

--- duelDuo.test.ts
+++ duelDuo.test.ts
@@ -27,15 +27,40 @@ test('Check Draw button displays when click', async () => {
     expect(choicesDiv).not.toBe('')
 
 })
-//kyles example
-test('Select a bot displays in our new div', async () => {
+// //kyles example
+// test('Select a bot displays in our new div', async () => {
 
+//     await driver.findElement(By.id('draw')).click()
+//     await driver.sleep(3000)
+//     await driver.findElement(By.xpath('//*[text()="Add to Duo"])[1]')).click()
+
+//     const palyerDuoDiv = await driver.findElement(By.id('player-duo'))
+//     const displayed = await palyerDuoDiv.isDisplayed()
+
+//     expect(displayed).toBe(true)
+// })
+
+
+//didn't relize i was writing the same test as klye til i was halfway through
+// test('Check to see if the Add to Duo will work', async () => {
+    
+//     await driver.findElement(By.id('draw')).click()
+//     await driver.sleep(300)
+//     await driver.findElement(By.xpath('(//*[text()="Add to Duo"])[1]')).click()
+
+//     const
+
+// })
+
+//still working on this test to check and see if the robots get removed 
+
+test('to check and see if bot is removed', async () => {
+    
     await driver.findElement(By.id('draw')).click()
-    await driver.sleep(300)
-    await driver.findElement(By.xpath('//*[text()="Add to Duo"])[1]')).click()
+    await driver.sleep(3000)
+    await driver.findElement(By.xpath('(//*[text()="Add to Duo"])[1]')).click()
 
-    const palyerDuoDiv = await driver.findElement(By.id('player-duo'))
-    const displayed = await palyerDuoDiv.isDisplayed()
-
-    expect(displayed).toBe(true)
+    const removeBot = driver.findElement(By.xpath('(//*[text()="Remove from Duo])[1]')).click()
+    
+    expect(removeBot).toBe(true)
 })
